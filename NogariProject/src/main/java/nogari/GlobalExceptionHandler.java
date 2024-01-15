4b03b1dff20b0df84f5b5dcf293468d61dfe68f9
KOTAEU <kctaeu@gmail.com>
2024-01-15 14:00:04 +0900

    add exception

---
+++ NogariProject/src/main/java/nogari/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package nogari;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+    	log.error("@@Exeption Handle");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Exeption Handle");
+    }
+}
