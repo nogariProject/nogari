90399d87a22b8c4b803b75d9e295ca1e96b15ef0
KJW <em221@naver.com>
2024-01-25 14:48:41 +0900

    SampleAop

--- NogariProject/src/main/java/nogari/global/config/AopConfig.java
+++ NogariProject/src/main/java/nogari/global/config/AopConfig.java
@@ -1,5 +1,33 @@
 package nogari.global.config;
 
-public class AopConfig {
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+/**  AOP용어
+ *   Advice    : 적용 할 공통로직(기능)
+ *   Joinpoint : Advice가 적용 될 수 있는 시점들
+ * 	 Pointcut  : Joinpoint 중 Advice가 실제로 적욜 될 시점들 묶음
+ *   Advisor   : Advice + Pointcut
+ *   Target    : Advisor를 적용할 대상객체
+ *   Aspect    : Target들에 적용 될 Advisor의 모음
+ *   Proxy     : Target에 Advisor가 적용 된 후 생성 된 객체
+ **/
+
+@Aspect
+@Component
+@Slf4j
+public class AopConfig {
+	
+	@Pointcut("@within(org.springframework.web.bind.annotation.RestControllerAdvice)")
+	private void exceptionLogPoint() {}	// @RestControllerAdvice 어노테이션이 붙은 클래스
+	
+	@Before("exceptionLogPoint()")
+	public void beforException(JoinPoint joinpoint) {
+		log.info("beforException :: "+ joinpoint.toString());
+	}
 }
--- NogariProject/src/main/java/nogari/global/error/GlobalExceptionHandler.java
+++ NogariProject/src/main/java/nogari/global/error/GlobalExceptionHandler.java
@@ -3,14 +3,13 @@ package nogari.global.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 	
 	/*  사용자의 요청은 error가 났기 때문에 HttpStatus.NOT_FOUND 등 에러정보를 ErrorResponse 객체(response)에 담고,
@@ -19,6 +18,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<ErrorResponse> normalExceptionHandler(MethodArgumentNotValidException ex) {
 		
+		log.info("AOP CHK :: @@@@@@@@@@@@@@@@@");
 		// of객체에서 인스턴스를 생성해서 반환해 주므로 final선언이 가능(변조방지)
 		final ErrorResponse response = ErrorResponse.of(HttpStatus.NOT_FOUND, "test error message", ex.getBindingResult());
 		
