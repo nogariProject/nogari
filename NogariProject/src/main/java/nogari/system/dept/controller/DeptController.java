ae2d4de368f483c4f3a2e85b0aa70d017ae647b6
Reading-Snail <readingsnail17@gmail.com>
2024-01-31 11:18:16 +0900

    dept controller edit

--- NogariProject/src/main/java/nogari/system/dept/controller/DeptController.java
+++ NogariProject/src/main/java/nogari/system/dept/controller/DeptController.java
@@ -13,22 +13,49 @@ import java.util.List;
 public class DeptController {
 
     private final DeptService deptService;
+
+    /**
+     * 부서 전체 리스트를 Connected By를 사용하여 조회
+     * @return List
+     */
     @GetMapping("")
     public List<DeptDTO> deptList(){
         return deptService.findDepts();
     }
+
+    /**
+     * 부서코드를 기준으로 부서 단건 조회
+     * @param deptCd
+     * @return
+     */
     @GetMapping("{deptCd}")
     public DeptDTO deptDetail(@PathVariable String deptCd){
         return deptService.findDeptByDeptCd(deptCd);
     }
+
+    /**
+     * 부서 추가
+     * @param deptDto
+     */
     @PostMapping("")
     public void deptAdd(DeptDTO deptDto){
         deptService.createDept(deptDto);
     }
+
+    /**
+     * 부서 정보 수정
+     * @param deptCd
+     * @param deptDto
+     */
     @PutMapping("/{deptCd}")
     public void deptModify(@PathVariable String deptCd, @RequestBody DeptDTO deptDto){
         deptService.editDept(deptDto);
     }
+
+    /**
+     * 부서 정보 삭제
+     * @param deptCd
+     */
     @DeleteMapping
     public void deptRemove(@PathVariable String deptCd){
         deptService.deleteDept(deptCd);
