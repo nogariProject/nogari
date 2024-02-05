063ac819ef55b5dd97b0fed0279cbb2a8351bc98
Yunseol <yseolcoding@gmail.com>
2024-02-05 16:13:19 +0900

    Add Func: Select BoardDetail to NtbdController

--- NogariProject/src/main/java/nogari/system/ntbd/controller/NtbdController.java
+++ NogariProject/src/main/java/nogari/system/ntbd/controller/NtbdController.java
@@ -1,26 +1,26 @@
 package nogari.system.ntbd.controller;
 
+import lombok.RequiredArgsConstructor;
 import nogari.system.ntbd.domain.dto.BoardRespDto;
 import nogari.system.ntbd.service.NtbdService;
+import nogari.system.ntbd.service.NtbdServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/board")
+@RequiredArgsConstructor
 public class NtbdController {
 
     private final NtbdService ntbdService;
 
     //게시글 내용 조회
-    @GetMapping("/{ntbdCd}")
-    public ResponseEntity<List<BoardRespDto>> selectBoard(@RequestParam String ntbdCd) throws Exception{
-        List<BoardRespDto> board = service.selectBoard(ntbdCd);
+    @GetMapping
+    public ResponseEntity<List<BoardRespDto>> selectBoard(@RequestParam("ntbdCd") String ntbdCd) throws Exception{
+        List<BoardRespDto> board = ntbdService.selectBoard(ntbdCd);
         return new ResponseEntity<>(board, HttpStatus.OK);
     }
 
