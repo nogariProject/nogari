81d9653c6095ef51e1821e62b131062ea816fa46
Yunseol <yseolcoding@gmail.com>
2024-02-06 09:23:03 +0900

    Modify ServiceImpl Transactional, DI 추가

--- NogariProject/src/main/java/nogari/system/ntbd/service/NtbdServiceImpl.java
+++ NogariProject/src/main/java/nogari/system/ntbd/service/NtbdServiceImpl.java
@@ -1,16 +1,20 @@
 package nogari.system.ntbd.service;
 
+import lombok.RequiredArgsConstructor;
 import nogari.system.ntbd.dao.mapper.NtbdMapper;
 import nogari.system.ntbd.domain.dto.BoardRespDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class NtbdServiceImpl implements NtbdService {
-    @Resource
-    private NtbdMapper mapper;
+
+    private final NtbdMapper mapper;
 
     public List<BoardRespDto> selectBoard(String ntbdCd){
         return mapper.selectBoard(ntbdCd);
