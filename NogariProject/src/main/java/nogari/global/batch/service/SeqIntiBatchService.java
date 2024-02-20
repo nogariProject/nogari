dd1705b4069ab48f36012b0ffa93304d9f94efc7
Reading-Snail <readingsnail17@gmail.com>
2024-02-20 11:17:31 +0900

    Refeash to apply new gitignore

--- NogariProject/src/main/java/nogari/global/batch/service/SeqIntiBatchService.java
+++
@@ -1,9 +0,0 @@
-package nogari.global.batch.service;
-
-import nogari.global.batch.domain.dto.SeqInitDTO;
-
-public interface SeqIntiBatchService {
-
-
-    SeqInitDTO seqInitBatExc() throws Exception;
-}// end class
--- NogariProject/src/main/java/nogari/system/log/dao/repository/ErrLogRepository.java
+++
@@ -1,9 +0,0 @@
-package nogari.system.log.dao.repository;
-
-import nogari.system.log.domain.entity.ErrLog;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ErrLogRepository extends CrudRepository<ErrLog,String> {
-}
